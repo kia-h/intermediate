31fb91ead6f67eb5d154eba68cd50ec508ce6ea9
kia-h <mhey237@gmail.com>
2016-05-17 17:29:36 +1000

    tutorial-13:Template Level Subscriptions

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,4 +1,10 @@
-Meteor.subscribe('recipes');
+
+Template.Recipes.onCreated(function(){
+  var self = this;
+  self.autorun(function(){
+    self.subscribe('recipes');
+  });
+});
 
 Template.Recipes.helpers({
   recipes: ()=> {
