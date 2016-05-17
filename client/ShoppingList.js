0db439c6ab385584127a4bce29f2bf1a02cef820
kia-h <mhey237@gmail.com>
2016-05-18 00:50:37 +1000

    tutorial-21:Building The Shopping List + some bug fix on add/remove menu buttons

---
+++ client/ShoppingList.js
@@ -0,0 +1,13 @@
+
+Template.ShoppingList.onCreated(function(){
+  var self = this;
+  self.autorun(function(){
+    self.subscribe('recipes');
+  });
+});
+
+Template.ShoppingList.helpers({
+  shoppingList: ()=> {
+    return Recipes.find({inMenu: true});
+  }
+});
