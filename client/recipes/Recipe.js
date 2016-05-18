93dbcf284b30d7cb271754080e925c4ec59e66aa
kia-h <mhey237@gmail.com>
2016-05-18 13:21:01 +1000

    tutorial-23:Deleting Recipes and Session Helpers

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -7,5 +7,13 @@ Template.Recipe.helpers({
 Template.Recipe.events({
   'click .toggle-menu' : function(){
     Meteor.call('toggleMenuItem',this._id, this.inMenu);
+  },
+
+  'click .fa-trash': function() {
+    Meteor.call('deleteRecipe',this._id);
+  },
+
+   'click .fa-pencil': function() {
+    Session.set('editMode', !Session.get('editMode'));
   }
 });
--- collections/Recipes.js
+++ collections/Recipes.js
@@ -67,6 +67,10 @@ Meteor.methods({
         inMenu: !currentState
       }
     });  
+  },
+
+  deleteRecipe: function(id) {
+    Recipes.remove(id);
   }
 });
 
